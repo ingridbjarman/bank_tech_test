d8fd1c0f396b9cdfa97efda4e1de4a31eb22abe0
Ingrid Bjarman <ingridbjarman@Makerss-MacBook-Pro-6.local>
2019-12-18 16:50:04 +0000

    Transaction_record object is initialised with an amount deposited property

--- spec/transaction_record.rb
+++ spec/transaction_record.rb
@@ -1,8 +1,9 @@
 class Transaction_record
 
-  attr_reader :date
-  def initialize(date)
+  attr_reader :date, :amount_deposited
+  def initialize(date, amount_deposited)
     @date = date
+    @amount_deposited = amount_deposited
   end
 
 end
--- spec/transaction_record_spec.rb
+++ spec/transaction_record_spec.rb
@@ -1,8 +1,12 @@
 require 'transaction_record'
 
 describe 'transaction_record' do
-  subject = Transaction_record.new(Time.now.strftime("%d-%m-%Y"))
+  subject = Transaction_record.new(Time.now.strftime("%d-%m-%Y"), 100)
   it 'initialises with a date property' do
     expect(subject.date).to eq "18-12-2019"
+  end
+
+  it 'initialises with an amount deposited property' do
+    expect(subject.amount_deposited).to eq 100
   end
 end
